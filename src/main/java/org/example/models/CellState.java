1d9413026749e1c6a570285a2726c0e00059ff83
Kassandra <alinka.makarova.2000@bk.ru>
2025-11-11 19:32:36 +0300

    состояние клетки на доске

--- src/main/java/org/example/models/CellState.java
+++ src/main/java/org/example/models/CellState.java
@@ -1,10 +1,39 @@
 package org.example.models;
 
 public enum CellState {
-    EMPTY,
-    SHIP,
-    MINE,
-    MISS,
-    HIT,
-    DESTROYED;
+    EMPTY("\uD83C\uDF0A"),
+    SHIP("\uD83C\uDF0A"),
+    MINE("\uD83D\uDCA3"),
+    MISS("❌"),
+    HIT("\uD83D\uDCA5"),
+    DESTROYED("✴");
+
+    private final String symbol;
+
+    CellState(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    /**
+     * Проверка на атаку клетки
+     * @return true - если можно атаковать, false - если нельзя
+     */
+    public boolean canAttack() {
+        return this == EMPTY || this == SHIP || this == MINE;
+    }
+
+    /**
+     * Проверка на видимость противнику
+     * @return true - если, MISS, HIT или DESTROYED, false - если EMPTY, SHIP или MINE
+     */
+    public boolean isVisible() {
+        return this == MISS || this == HIT || this == DESTROYED;
+    }
+
+
+
 }
