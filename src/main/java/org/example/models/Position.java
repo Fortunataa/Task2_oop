bb5db3c48cfc501b27c0c30b14f728aa147bdba1
Kassandra <alinka.makarova.2000@bk.ru>
2025-11-11 17:55:55 +0300

    реализация класса позиции

---
+++ src/main/java/org/example/models/Position.java
@@ -0,0 +1,58 @@
+package org.example.models;
+
+public class Position {
+    private final int x;
+    private final int y;
+
+    public Position(int x, int y) {
+        if (x < 0 || y < 0) {
+            throw new IllegalArgumentException("Координаты не могу быть меньше 0: x = " + x + ", y = " + y);
+        }
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+
+    /**
+     * Проверяет, находится ли точка в поле координат
+     * @param boardSize размер поля
+     * @return true, если точка находится внутри поля
+     */
+    public boolean isInPlayingField(int boardSize) {
+        return x >= 0 && x < boardSize && y >= 0 && y < boardSize;
+    }
+
+    /**
+     * Создает новую позицию со смещением
+     * @param dx смещение по X
+     * @param dy смещение по Y
+     * @return новая позиция Position(x + dx, y + dy)
+     */
+    public Position createNewPosition(int dx, int dy) {
+        return new Position(x + dx, y + dy);
+    }
+
+    public Position left() {
+        return new Position(x - 1, y);
+    }
+
+    public Position right() {
+        return new Position(x + 1, y);
+    }
+
+    public Position up() {
+        return new Position(x, y - 1);
+    }
+
+    public Position down() {
+        return new Position(x, y + 1);
+    }
+}
