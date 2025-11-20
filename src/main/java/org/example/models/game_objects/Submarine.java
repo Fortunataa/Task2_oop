f4107217b7c35a16f6af07129160e7d3bdf997d0
MakarovaA <alinka.makarova.2000@bk.ru>
2025-11-20 11:28:12 +0300

    Класс подводной лодки

--- src/main/java/org/example/models/game_objects/Submarine.java
+++ src/main/java/org/example/models/game_objects/Submarine.java
@@ -16,6 +16,11 @@ public class Submarine extends GameObject {
 
     }
 
+    /**
+     * Вычисляет позиции подводной лодки
+     * @param start начало лодки, ее "носик"
+     * @return список клеток, которые занимает подводная лодка
+     */
     private List<Position> calculateHorizontalPositions(Position start) {
         List<Position> positions = new ArrayList<>();
 
@@ -26,6 +31,12 @@ public class Submarine extends GameObject {
         return positions;
     }
 
+    /**
+     * Проверяет, можно ли разместить подводную лодку
+     * @param position позиция клетки
+     * @param board игровая доска
+     * @return true - если лодку можно разместить (она находится в поле), false - если нельзя (за пределами поля)
+     */
     @Override
     public boolean canBePlacedObject(Position position, GameBoard board) {
         List<Position> testPositions = calculateHorizontalPositions(position);
@@ -36,5 +47,37 @@ public class Submarine extends GameObject {
             }
         }
         return true;
+    }
+
+    @Override
+    public boolean isDestroyed() {
+        return used;
+    }
+
+    public List<Position> getAllPositions() {
+        return new ArrayList<>(allPositions);
+    }
+
+    public boolean isUsed() {
+        return used;
+    }
+
+    /**
+     * Отмечает подлодку как использованную (самоуничтожение)
+     */
+    public void markAsUsed() {
+        this.used = true;
+    }
+
+    /**
+     * Проверяет, можно ли использовать подлодку
+     */
+    public boolean canBeUsed() {
+        return !used;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Подлодка [позиции = %s, использована = %s]", allPositions, used);
     }
 }
